21be469034e33ff29a384e4d6507b52b91d6c0f6
khanhbq_he2002 <khanhbui1560@gmail.com>
2023-11-07 17:07:32 +0700

    Update Character Animation Logic

--- Assets/_Game/Scripts/Models/CharacterBase.cs
+++ Assets/_Game/Scripts/Models/CharacterBase.cs
@@ -6,12 +6,22 @@ public class CharacterBase : MonoBehaviour
     [SerializeField] protected Animator _animator;
     [SerializeField] protected Transform _weaponHolder;
     [SerializeField] protected Rigidbody _rigidbody;
+
     [Header("Character Stats"), Space(5f)]
     [SerializeField] protected float _rotateSpeed;
     [SerializeField] protected float _moveSpeed;
+
+    protected static string IDLE_ANIMATION = "IsIdle";
+    protected static string WIN_ANIMATION = "IsWin";
+    protected static string ATTACK_ANIMATION = "IsAttack";
+    protected static string DEAD_ANIMATION = "IsDead";
+    protected static string DANCE_ANIMATION = "IsDead";
+    protected static string ULTI_ANIMATION = "IsUlti";
 
     protected bool _isIdle = true;
     protected bool _isDead = false;
     protected bool _isWin = false;
     protected bool _isAttack = false;
+    protected bool _isDance = false;
+    protected bool _isUlti = false;
 }
--- Assets/_Game/Scripts/Models/Player.cs
+++ Assets/_Game/Scripts/Models/Player.cs
@@ -2,32 +2,75 @@ using UnityEngine;
 
 public class Player : CharacterBase
 {
+    private float _horizontal;
+    private float _vertical;
+    private Vector3 _direction;
+    private bool _inAttackProcess = false;
+
     private void FixedUpdate()
     {
+        PlayerInput();
         Movement();
+        Attack();
+        SetAnimationParameters();
     }
+
+    /// <summary>
+    /// Listen to player input.
+    /// </summary>
+    private void PlayerInput()
+    {
+        if (!_inAttackProcess)
+        {
+            _isAttack = Input.GetMouseButtonDown(0);
+        }
+        _horizontal = Input.GetAxis("Horizontal");
+        _vertical = Input.GetAxis("Vertical");
+        _direction = new Vector3(_horizontal, 0f, _vertical).normalized;
+        _isIdle = _direction == Vector3.zero;
+    }
+
     /// <summary>
     /// Controls the movement of the character.
     /// </summary>
     private void Movement()
     {
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
+        if (_isAttack || _isIdle || _isWin || _isDead) return;
 
-        Vector3 direction = new Vector3(horizontal, 0f, vertical);
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(_direction), _rotateSpeed);
+        transform.position = Vector3.Lerp(transform.position, transform.position + _direction, _moveSpeed * Time.fixedDeltaTime);
+    }
 
-        if (direction == Vector3.zero)
-        {
-            _animator.SetBool("IsIdle", true);
-            transform.Translate(Vector3.zero);
-        }
-        else
-        {
-            _animator.SetBool("IsIdle", false);
-            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(direction), _rotateSpeed);
-            transform.position = Vector3.Lerp(transform.position, transform.position + direction.normalized, _moveSpeed * Time.fixedDeltaTime);
-        }
+    /// <summary>
+    /// Controls the attack of the character.
+    /// </summary>
+    private void Attack()
+    {
+        if (!_isAttack || _inAttackProcess || _isDead || _isWin || _isUlti) return;
+        _inAttackProcess = true;
+        Invoke(nameof(EndAttackProcess), .65f);
     }
 
 
+    /// <summary>
+    /// Sets the animation parameters.
+    /// </summary>
+    private void SetAnimationParameters()
+    {
+        _animator.SetBool(IDLE_ANIMATION, _isIdle);
+        _animator.SetBool(WIN_ANIMATION, _isWin);
+        _animator.SetBool(DEAD_ANIMATION, _isDead);
+        _animator.SetBool(DANCE_ANIMATION, _isDance);
+        _animator.SetBool(ULTI_ANIMATION, _isUlti);
+        _animator.SetBool(ATTACK_ANIMATION, _isAttack);
+    }
+
+    /// <summary>
+    /// The end of the attack process.
+    /// </summary>
+    private void EndAttackProcess()
+    {
+        _inAttackProcess = false;
+        _isAttack = false;
+    }
 }
